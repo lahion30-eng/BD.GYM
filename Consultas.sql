f52ab6703ddb61dd0b86a07ecfcb197bdb2e04c0
lahion30-eng <lahion30@gmail.com>
2026-03-10 23:36:13 -0400

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -2,68 +2,62 @@
 PRAGMA foreign_keys = ON;
 
 -- creamos una tabla para las clases
-CREATE TABLE Clases (
+CREATE TABLE Clase (
     id_clase INTEGER PRIMARY KEY,
     nombre_clase TEXT NOT NULL,
-    dia TEXT NOT NULL,
+    dia_semana TEXT NOT NULL,
     horario TEXT NOT NULL
 );
 
 -- tabla para los miembros del gym
-CREATE TABLE Miembros (
+CREATE TABLE Miembro (
     cedula INTEGER PRIMARY KEY,
-    nombre TEXT NOT NULL,
-    apellido TEXT NOT NULL,
+    nombre_completo TEXT NOT NULL,
     telefono TEXT NOT NULL
 );
 
 -- esta es la tabla intermedia porque tenemos una relacion de muchos a muchos N:M
 CREATE TABLE IF NOT EXISTS Inscripcion (
-    id_inscripcion INTEGER PRIMARY KEY,
-    cedula INTEGER NOT NULL,
-    id_clase INTEGER NOT NULL,
-    FOREIGN KEY (cedula) REFERENCES Miembros (cedula),
-    FOREIGN KEY (id_clase) REFERENCES Clases (id_clase)
+    cedula INTEGER,
+    id_clase INTEGER,
+    PRIMARY KEY (cedula, id_clase),
+    FOREIGN KEY (cedula) REFERENCES Miembro (cedula),
+    FOREIGN KEY (id_clase) REFERENCES Clase (id_clase)
 );
 
 Insertar Miembros
 INSERT INTO
-    Miembros (cedula, nombre, apellido, telefono)
+    Miembro (cedula, nombre_completo, telefono)
 VALUES (
         '40212345678',
-        'Juan',
-        'Soto',
+        'Juan Soto',
         '8093457821'
 
     ),
     (
         '00123456789',
-        'Julio',
-        'Rodriguez',
+        'Julio Rodriguez',
         '8296149053'
     ),
     (
         '03198765432',
-        'Vladimir',
-        'Guerrero',
+        'Vladimir Guerrero',
         '8492734689'
     ),
     (
         '40287654321',
-        'Rafael',
-        'Devers',
+        'Rafael Devers',
         '8095561294'
     ),
     (
         '22345678901',
-        'Fernando',
-        'Tatis',
+        'Fernando Tatis',
         '8494465081'
     );
 
 Insertar Clases
 INSERT INTO
-    Clases (id_clase, nombre_clase, dia, horario)
+    Clase (id_clase, nombre_clase, dia_semana, horario)
 VALUES (
         '401',
         'Boxeo Fitness',
@@ -79,7 +73,7 @@ VALUES (
     ),
     (
         '403',
-        'Bachata',
+        'Yoga',
         'Miercoles',
         '5 a 6 p.m'
     ),
@@ -91,27 +85,30 @@ VALUES (
     ),
     (
         '405',
-        'Yoga',
+        'Bachata',
         'Viernes',
         '6 a 7 p.m'
     );
 
 Insertar Inscripciones
-INSERT INTO Inscripcion (id_inscripcion, cedula, id_clase)
+INSERT INTO Inscripcion (cedula, id_clase)
 VALUES
-(1, 40212345678, 401),
-(2, 00123456789, 402),
-(3, 03198765432, 403),
-(4, 40287654321, 404),
-(5, 22345678901, 405);
+(40212345678, 401),
+(00123456789, 402),
+(03198765432, 403),
+(40287654321, 404),
+(22345678901, 405);
 
 Consulta con tablas relacionadas
 SELECT
-    Miembros.cedula,
-    Miembros.nombre,
-    Miembros.apellido,
-    Inscripcion.id_inscripcion,
-    Inscripcion.id_clase
-FROM Miembros
+    Miembro.cedula,
+    Miembro.nombre_completo,
+    Clase.nombre_clase,
+    Clase.dia_semana,
+    Clase.horario
+FROM Miembro
 INNER JOIN Inscripcion
-ON Miembros.cedula = Inscripcion.cedula;
+ON Miembro.cedula = Inscripcion.cedula
+INNER JOIN Clase
+ON Clase.id_clase = Inscripcion.id_clase;
+
